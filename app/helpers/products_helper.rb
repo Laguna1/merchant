d7f8fdd259d650083d88596e3d477fe674d32d76
Laguna1 <lagunazoomarket@gmail.com>
2021-11-26 22:21:49 +0200

    Fix print_stock method

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -10,10 +10,10 @@ module ProductsHelper
   end
 
   def print_stock(stock, requested = 1)
-    if stock < requested
-      content_tag(:span, 'Out of Stock')
-    elsif stock > requested
-      content_tag(:span, "In Stock (#{stock}) ")
+    if stock == 0
+      content_tag(:span, 'Out of Stock', class: "out_stock")
+    elsif stock >= requested
+      content_tag(:span, "In Stock (#{stock}) ", class: "in_stock")
     else
       content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
     end
