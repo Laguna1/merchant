d1711b7bd9b6c2c8f9f4c43f74df5ded39322ffc
Laguna1 <lagunazoomarket@gmail.com>
2021-11-26 09:05:55 +0200

    Fix rubocop offenses

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,5 +2,5 @@
 
 class Product < ApplicationRecord
   validates_numericality_of :price
-  validates_numericality_of :stock, only_integer: true, greater_than_or_equal_to: 0
+  validates :stock, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
 end
