d5eacd478638ba97de9b3ffa005ba296e754bad8
Laguna1 <lagunazoomarket@gmail.com>
2021-11-26 21:46:39 +0200

    Confirm that setting the quantity to zero removes an item from the order

--- app/controllers/order_items_controller.rb
+++ app/controllers/order_items_controller.rb
@@ -1,7 +1,7 @@
 # frozen_string_literal: true
 
 class OrderItemsController < ApplicationController
-  before_action :set_order_item, only: %i[show edit update destroy]
+  before_action :set_order_item, only: %i[show edit destroy]
   before_action :load_order, only: :create
   # GET /order_items/1/edit
   def edit; end
