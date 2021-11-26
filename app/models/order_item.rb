6b157551dc82afdac0c392de659be03c10345944
Laguna1 <lagunazoomarket@gmail.com>
2021-11-26 21:28:38 +0200

    Add validations for order_items quantity greater than zero

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -6,6 +6,7 @@ class OrderItem < ApplicationRecord
 
   validates :order_id, presence: true
   validates :product_id, presence: true
+  validates :quantity, numericality: { only_integer: true, greater_than: 0 }
 
   def subtotal
     quantity * product.price
