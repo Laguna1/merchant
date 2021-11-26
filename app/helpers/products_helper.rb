c61357a1be63de4dfc085cde52561715232aa5fb
Laguna1 <lagunazoomarket@gmail.com>
2021-11-26 22:06:52 +0200

    Modify print_stock method

--- app/helpers/products_helper.rb
+++ app/helpers/products_helper.rb
@@ -9,11 +9,13 @@ module ProductsHelper
     number_to_currency price
   end
 
-  def print_stock(stock)
-    if stock.positive?
-      content_tag(:span, "Stock (#{stock}) ")
-    else
+  def print_stock(stock, requested = 1)
+    if stock < requested
       content_tag(:span, 'Out of Stock')
+    elsif stock > requested
+      content_tag(:span, "In Stock (#{stock}) ")
+    else
+      content_tag(:span, "Insufficient stock (#{stock})", class: "low_stock")
     end
   end
 end
