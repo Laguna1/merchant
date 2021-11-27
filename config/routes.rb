ae3f02b8f1488cf0c6854d823af8c28facb2efd1
Laguna1 <lagunazoomarket@gmail.com>
2021-11-27 22:04:48 +0200

    Update routes - login, logout

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   match '/auth/:provider/callback', to: 'sessions#create', via: 'get'
   match '/auth/failure', to: redirect('/'), via: 'get'
-  
+  match "/login" => redirect("/auth/twitter"), as: :login, via: :get
+  match "/logout" => "sessions#destroy", as: :logout, via: :get
+
   resources :orders
   resources :order_items
   resources :products
