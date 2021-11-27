cb30f8e2e60d6d159d817b428635138a44a80176
Laguna1 <lagunazoomarket@gmail.com>
2021-11-27 21:47:10 +0200

    Create action redirection after log in

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,5 +1,7 @@
 class SessionsController < ApplicationController
   def create
-    render text: request.env["omniauth.auth"]
+    @user = User.find_or_create_by_auth(request.env["omniauth.auth"])
+  session[:user_id] = @user.id
+  redirect_to products_path, notice: "Logged in as #{@user.name}"
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def self.find_or_create_by_auth(auth_data)
+  find_or_create_by(provider: auth_data["provider"], uid: auth_data["uid"], name: auth_data["info"]["name"])
+  end
 end
