ba9f1f014e3eb80577368a95dfd175603e5320fe
Laguna1 <lagunazoomarket@gmail.com>
2021-11-27 22:10:55 +0200

    Imllement user log out

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,9 @@ class SessionsController < ApplicationController
   session[:user_id] = @user.id
   redirect_to products_path, notice: "Logged in as #{@user.name}"
   end
+
+  def destroy
+    session.destroy
+    redirect_to root_path, notice: 'Goodbye!!!'
+end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,6 @@ Rails.application.routes.draw do
   resources :orders
   resources :order_items
   resources :products
-  
+  root 'products#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
