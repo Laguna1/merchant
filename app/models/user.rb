fbdfe2e66197d58cf70f88e9a89aa0718c365f17
Laguna1 <lagunazoomarket@gmail.com>
2021-11-28 22:28:56 +0200

    Display username in order page

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,8 @@
 # frozen_string_literal: true
 
 class User < ApplicationRecord
+  has_many :orders
+
   def self.find_or_create_by_auth(auth_data)
     find_or_create_by(provider: auth_data['provider'], uid: auth_data['uid'], name: auth_data['name'])
   end
